bea54307a081bafec15cacc3fa9619653a7bcb8f
Chris Barts <chbarts@gmail.com>
2016-03-15 22:07:31 -0600

    First pass at XHR file upload code.

--- chromePaw/src/RestFacade.js
+++ chromePaw/src/RestFacade.js
@@ -62,9 +62,39 @@ function listFiles(folderId)
 
 }
 
-function insertFile() {
+function insertFile(fileName, fileData, callback, onerror) {
 
+    var boundary = '01234567890123456789';
+    var metaData = { 'name' : fileName };
+    var accessTokan = gapi.auth.getToken().access_token;
+    var xhr = new XMLHttpRequest('POST', 'https://www.googleapis.com/upload/drive/v3/files?uploadType=multipart');
+    xhr.setRequestHeader('Authorization', 'Bearer ' + accessToken);
+    xhr.setRequestHeader('Content-Type', 'multipart/related; boundary=' + boundary);
+    var request =
+        '--' + boundary + '\n'
+        + 'Content-Type: application/json; charset=UTF-8\n\n'
+        + JSON.stringify(metaData)
+        + '\n\n'
+        + '--' + boundary + '\n'
+        + 'Content-Type: application/json; charset=UTF-8\n\n'
+        + JSON.stringify(fileData) + '\n'
+        + boundary + '--';
+    xhr.setRequestHeader('Content-Length', request.length);
+    xhr.onload = function(res) {
+        if (xhr.readyState === 4) {
+            if (xhr.status === 200) {
+                callback(xhr.responseText);
+            } else {
+                onerror(xhr.statusText);
+            }
+        }
+    };
 
+    xhr.onerror = function(res) {
+        onerror(xhr.statusText);
+    };
+
+    xhr.send(request);
 
 }
 
