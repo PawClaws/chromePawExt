a77bcf009ec89b11ef96467aa5573c299333e2e5
Matthew Dolan <mdolan2424@gmail.com>
2016-04-11 17:32:34 -0600

    Integrated Chris Query Functions into restFacade.

--- chromePaw/src/RestFacade.js
+++ chromePaw/src/RestFacade.js
@@ -1,5 +1,5 @@
 
-function insertFile(fileData,token) {
+function insertFileRoot(fileData,token) {
     const boundary = '-------314159265358979323846';
     const delimiter = "\r\n--" + boundary + "\r\n";
     const close_delim = "\r\n--" + boundary + "--";
@@ -45,8 +45,112 @@ function insertFile(fileData,token) {
         request.execute(callback);
     }
 }
+
+function insertFile_(folderId,fileData,token,callback) {
+    const boundary = '-------314159265358979323846';
+    const delimiter = "\r\n--" + boundary + "\r\n";
+    const close_delim = "\r\n--" + boundary + "--";
+
+    var reader = new FileReader();
+    reader.readAsBinaryString(fileData);
+    reader.onload = function(e) {
+        var contentType = fileData.type || 'application/octet-stream';
+        var metadata = {
+            'title': fileData.fileName,
+            'mimeType': contentType
+        };
+
+        if (folderId) {
+            metadata['parents'] = [folderId];
+        }
+
+        var base64Data = btoa(reader.result);
+        var multipartRequestBody =
+            delimiter +
+            'Content-Type: application/json\r\n\r\n' +
+            JSON.stringify(metadata) +
+            delimiter +
+            'Content-Type: ' + contentType + '\r\n' +
+            'Content-Transfer-Encoding: base64\r\n' +
+            '\r\n' +
+            base64Data +
+            close_delim;
+
+        var accessTokenObj = {};
+        accessTokenObj.access_token = token;
+        accessTokenObj.token_type = "Bearer";
+        accessTokenObj.expires_in = "3600";
+        gapi.auth.setToken(accessTokenObj);
+
+        var request = gapi.client.request({
+            'path': '/upload/drive/v2/files',
+            'method': 'POST',
+            'params': {'uploadType': 'multipart'},
+            'headers': {
+                'Content-Type': 'multipart/mixed; boundary="' + boundary + '"'
+            },
+            'body': multipartRequestBody});
+        if (!callback) {
+            callback = function(file) {
+                console.log(file)
+            };
+        }
+        request.execute(callback);
+    }
+}
+
+function insertFile(fileData, token, callback) {
+    insertFile_(false, fileData, token, callback);
+}
+
+function insertFileIntoFolder(folderId, fileData, token, callback) {
+    insertFile_(folderId, fileData, token, callback);
+}
 //list files
-function listFiles(folderId, callback)
+function listFiles_(query, folderId, callback)
+{
+    gapi.client.load('drive', 'v2', function() {
+        var accessTokenObj = {};
+        accessTokenObj.access_token = gapi.auth.getToken().access_token;
+        accessTokenObj.token_type = "Bearer";
+        accessTokenObj.expires_in = "3600";
+        gapi.auth.setToken(accessTokenObj);
+
+        var retrievePageOfChildren = function(request, result) {
+            request.execute(function(resp) {
+                result = result.concat(resp.items);
+                var nextPageToken = resp.nextPageToken;
+                if (nextPageToken) {
+                    var ireq = {'pageToken': nextPageToken};
+                    if (folderId) { ireq['folderId'] = folderId; }
+                    request = gapi.client.drive.children.list(ireq);
+                    retrievePageOfChildren(request, result);
+                } else {
+                    callback(result);
+                }
+            });
+        }
+        var req = {};
+        if (folderId) {
+            req['folderId'] = folderId;
+        }
+
+        if (query) {
+            req['q'] = query;
+        }
+
+        var initialRequest = gapi.client.drive.children.list(req);
+        retrievePageOfChildren(initialRequest, []);
+
+
+
+
+
+    });
+}
+
+//list files
+function listFilesRoot(folderId, callback)
 {
     gapi.client.load('drive', 'v2', function() {
         var accessTokenObj = {};
@@ -130,4 +234,31 @@ function downloadFile(fileId,token) {
 
 }
 
+function createFolder(folderName, token, callback) {
+    var FOLDER_MIME_TYPE = "application/vnd.google-apps.folder";
+    var request = gapi.client.request({
 
+        'path': '/drive/v2/files/',
+        'method': 'POST',
+        'headers': {
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer ' + token,
+        },
+        'body':{
+            "title" : folderName,
+            "mimeType" : FOLDER_MIME_TYPE,
+        }
+    });
+
+    request.execute(callback);
+}
+
+
+function listFiles(folderId, callback) {
+    return listFiles_(false, folderId, callback);
+}
+
+function listFoldersByName(name, callback) {
+    var query = 'mimeType = '+ FOLDER_MIME_TYPE + ' and name contains ' + name;
+    return listFiles_(query, false, callback);
+}
